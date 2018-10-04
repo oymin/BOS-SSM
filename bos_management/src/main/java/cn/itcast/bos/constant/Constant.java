04bed7ae9d4d2de2318e5419987c3ee43fb3185e
ouyangmin <oymtofight@163.com>
2018-10-04 20:58:48 +0800

    delete file

--- bos_management/src/main/java/cn/itcast/bos/constant/Constant.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.bos.constant;
-
-public class Constant {
-	
-	//用户密码 md5加密次数值
-	public static final int MD5_HASH_ITERATIONS = 1024;
-	
-}
--- bos_management/src/main/resources/new.sql
+++
@@ -1,5 +0,0 @@
-create table customers(
-  id int PRIMARY KEY auto_increment,
-  name VARCHAR (20),
-  age INT
-);
